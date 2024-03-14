84ec121d863f85defd706996af881b00e3591454
FelipeFavato <felipelopesfavato@gmail.com>
2024-03-14 17:27:18 -0300

    Skeleton Loading added. Top NavBar added. Images added

--- frontend/src/components/MenuNav.vue
+++ frontend/src/components/MenuNav.vue
@@ -1,9 +1,82 @@
 <script>
+import { useRouter } from 'vue-router';
+
+export default {
+  data () {
+    return {
+      useRouter: useRouter(),
+    }
+  },
+
+  methods: {
+    // Redirect to loginPage /redireciona para a página de login.
+    redirectToLogin () {
+      this.useRouter.push('/login');
+    },
+    // Exclude Token and redirect to login/
+    // Exclui o token e redireciona para o login.
+    logout () {
+      localStorage.removeItem('token');
+      this.redirectToLogin();
+    },
+  }
+}
 </script>
 
 <template>
+  <!-- Top NavBar that should render on every page except for login page -->
+  <!-- Barra de navegação que deve renderizar em todas as paginas -->
+  <nav class="navbar navbar-expand-lg bg-body-tertiary margin-bottom" data-bs-theme="dark">
+    <div class="container-fluid">
 
+      <!-- Logo/Name -->
+      <div class="navbar-brand">
+        <p class="constr font-size">File uploader</p>
+      </div>
+
+      <!-- Cellphone navMenu /Menu navegação Celular -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      ><span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+      </div>
+
+      <div class="d-flex separate">
+        <!-- Logout button/ Botão - Sair -->
+        <button
+          type="button"
+          class="nav-link small"
+          title="Logout"
+          @click="logout"
+        ><img src="../assets/logout.png" alt="logout">
+        </button>
+      </div>
+    </div>
+
+  </nav>
 </template>
 
-<style>
+<style scoped>
+.font-size {
+  font-size: 20px;
+  height: 15px;
+}
+
+.small img {
+  height: 25px;
+  width: 25px;
+}
+
+.margin-bottom {
+  margin-bottom: 5px;
+}
 </style>
---
+++ frontend/src/components/SkeletonLoading/SkeletonCardsLoading.vue
@@ -0,0 +1,62 @@
+<template>
+  <main class=" flex">
+
+    <!-- Cards -->
+    <div class="row row-cols-1 row-cols-md-3 g-4 mt-1">
+
+      <div v-for="(arquivo, i) in 9" :key="i" class="col">
+        <div class="card loading-elements bigger-cards">
+          <div class="card-body flex-direction-row">
+              <h5 class="card-title loading-elements"></h5>
+              <div>
+                <button
+                class="btn loading-elements"
+                type="button"
+                >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </main>
+
+</template>
+
+<style>
+.loading-elements {
+  border-radius: 5px;
+  animation: pulse-bg 1.5s infinite, pulse-text 1.5s infinite;
+}
+
+.bigger-cards {
+  height: 300px;
+}
+
+@keyframes pulse-bg {
+  0% {
+    background-color: #f0f0f0;
+  }
+  50% {
+    background-color: #bcbcbc;
+  }
+  100% {
+    background-color: #f0f0f0;
+  }
+}
+
+@keyframes pulse-text {
+  0% {
+    color: #f0f0f0;
+  }
+  50% {
+    color: #bcbcbc;
+  }
+  100% {
+    color: #f0f0f0;
+  }
+}
+</style>
